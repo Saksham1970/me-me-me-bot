55b6f2730e17d113c606d959b6b8efedacc25446
Blonteractor <48821041+Blonteractor@users.noreply.github.com>
2019-12-29 17:02:00 +0530

    Please work Heroku.

--- general.py
+++ general.py
@@ -1,61 +1,61 @@
 import praw
 import os
 import discord
-from colorama import init, Fore, Back ,Style
+from colorama import init, Fore, Back, Style
 import json
 from git import Repo
 import git
 from datetime import datetime
 
-roles = ['Prostitute','Rookie','Adventurer','Player','Hero']
+roles = ['Prostitute', 'Rookie', 'Adventurer', 'Player', 'Hero']
 
-status = ["Saksham's Son", 'Is Mayank','Who Is Gay']
+status = ["Saksham's Son", 'Is Mayank', 'Who Is Gay']
 
-subreddits = ["memes","dankmemes","cursedcomments","animemes"]
+subreddits = ["memes", "dankmemes", "cursedcomments", "animemes"]
 
 epic = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
 
-
-admin_role_id = 632906375839744001 
+admin_role_id = 632906375839744001
 MEE6_disc = 4876
 
 
-level_Rookie=5
-level_Adventurer=10
-level_Player=25
-level_Hero=50
-level_CON=85
+level_Rookie = 5
+level_Adventurer = 10
+level_Player = 25
+level_Hero = 50
+level_CON = 85
 
 reddit = praw.Reddit(
-client_id = os.environ.get("REDDIT_CLIENT_ID"),
-client_secret=os.environ.get("REDDIT_CLIENT_SECRET"),
-username= os.environ.get("REDDIT_USERNAME"),
-password= os.environ.get("REDDIT_PASSWORD"),
-user_agent="FuqU"
+    client_id=os.environ.get("REDDIT_CLIENT_ID"),
+    client_secret=os.environ.get("REDDIT_CLIENT_SECRET"),
+    username=os.environ.get("REDDIT_USERNAME"),
+    password=os.environ.get("REDDIT_PASSWORD"),
+    user_agent="FuqU"
 )
 
 
+def commit(sp_msg: str()):
 
-def commit(sp_msg : str()):
+    os.rename("./Database/gothy", "./Database/.git")
 
-    os.rename("./Database/gothy","./Database/.git")
-    repo = Repo("./Database/.git")
     now = datetime.now()
     date_time = now.strftime("%d/%m/%Y %H:%M:%S")
     commit_msg = f"Database updated - {date_time} -> {sp_msg} "
-    
-    repo.git.add(update = True)
-    repo.index.commit(commit_msg)
-    
-    origin = repo.remote(name = "origin")
-    origin.push()
-    os.rename("./Database/.git","./Database/gothy")
+    g = git.Git("./Database")
+    try:
+        g.execute(f'git commit -a -m "{commit_msg}" ')
+        g.execute("git push")
+    except Exception as e:
+        print(e)
+    os.rename("./Database/.git", "./Database/gothy")
+
+
 def reset():
-    
-    origin = repo.remote(name = "origin")
+
+    origin = repo.remote(name="origin")
     origin.pull()
-    
+
     g = git.Git("./Database")
     g.execute("git stash")
 
@@ -66,17 +66,16 @@ def reset():
     print("Pulled Database Successfully")
 
 
-
 def permu(strs):
-    if len(strs)==1:
+    if len(strs) == 1:
         if strs.isalnum():
-            return [strs.lower(),strs.upper()]
+            return [strs.lower(), strs.upper()]
         else:
             return[strs]
     else:
         output = []
         f = strs[0]
-        l=strs[1:]
+        l = strs[1:]
         for st in permu(l):
             if f.isalnum():
                 output.append(f.lower() + st)
@@ -85,20 +84,25 @@ def permu(strs):
                 output.append(f+st)
         return output
 
+
 def error_message(error):
     init(convert=True)
     print(Fore.BLACK + Back.WHITE + str(error))
     print(Fore.WHITE+Back.BLACK)
-  
+
+
 def db_receive(name):
-    with open(f'./Database/{name}.json','r') as f:   
-        return json.load(f)                                                                                       
+    with open(f'./Database/{name}.json', 'r') as f:
+        return json.load(f)
 
-def db_update(name,db):
-    with open(f'./Database/{name}.json','w') as f:                                                                                            
-        json.dump(db,f)
 
-def new_entry(name,disc):
-    mem_info=db_receive("inf")
-    mem_info[disc] = {"name": name,"messages" : 0 , "level" : "Prostitute" , "coins" : 500}
-    db_update("inf",mem_info)
+def db_update(name, db):
+    with open(f'./Database/{name}.json', 'w') as f:
+        json.dump(db, f)
+
+
+def new_entry(name, disc):
+    mem_info = db_receive("inf")
+    mem_info[disc] = {"name": name, "messages": 0,
+                      "level": "Prostitute", "coins": 500}
+    db_update("inf", mem_info)
