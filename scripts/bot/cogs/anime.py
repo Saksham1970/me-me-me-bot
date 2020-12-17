d59b4c5ae7292427b1430f5694d667dbb49cb3f5
Priyanshu <priyanv2003@gmail.com>
2020-12-17 11:54:39 +0530

    Added the anime cog

---
+++ scripts/bot/cogs/anime.py
@@ -0,0 +1,151 @@
+import sys
+import os
+sys.path.append(os.path.abspath("./scripts/others/"))
+
+import discord
+import asyncio
+from random import choice
+from discord.ext import commands
+from math import floor
+from MAL import Anime as AnimeObject, MALConfig
+from dotenv import load_dotenv
+load_dotenv()
+
+class Anime(commands.Cog):
+      
+    config = MALConfig(
+    client_id = os.environ.get("MAL_CLIENT_ID"),
+    client_secret = os.environ.get("MAL_CLIENT_SECRET"),
+    access_token = os.environ.get("MAL_ACCESS_TOKEN"),
+    refresh_token = os.environ.get("MAL_REFRESH_TOKEN")
+    )
+    
+    MAL_LOGO = "https://upload.wikimedia.org/wikipedia/commons/7/7a/MyAnimeList_Logo.png"
+    
+    def __init__(self, client: commands.Bot) -> None:
+        self.client = client
+        
+    @staticmethod
+    def format_list(l) -> str:
+        result = ""
+        for i, j in enumerate(l):
+            if not i == len(l) - 1:
+                result += f"`{j}`, "
+            else:
+                result += f"`{j}`"
+                
+        return result
+        
+    @commands.command()
+    async def anime(self, ctx, *, query):
+        result = list(AnimeObject.search(query=query, limit=10, basic=True, config=Anime.config))
+        if len(result) == 0:
+            await ctx.send(f"No anime of name `{query}` found.")
+            return
+        
+        msg_content = "Respond with the index of the anime you want, 'c' to cancel\n"
+        for index, anime in enumerate(result):
+            anime_name = anime["name"]
+            msg_content += f"{index + 1}. **{anime_name}**\n"
+        
+        search_message = await ctx.send(msg_content)
+        
+        def check(message) -> bool:
+            return message.author == ctx.author
+        
+        the_chosen_id = None
+        
+        while True:
+            try:
+                response = await self.client.wait_for("message", check=check, timeout=30)
+            except asyncio.TimeoutError:
+                await ctx.send("I got no response sadly, try refining your search term if you did'nt find your anime.")
+                return
+            else:
+                response_content = response.content.lower()
+                
+                if response_content == "c":
+                    await search_message.edit(content="Command cancelled, try refining your search term if you did'nt find your anime.")
+                    break
+                
+                elif not response_content.isnumeric() or not floor(int(response_content)) == int(response_content) or int(response_content) <= 0:
+                    await ctx.send("Respond with a natural number, should have been obvious enuff.")
+                    continue
+                
+                elif int(response_content) > len(result):
+                    await ctx.send("I dont even have that many results bro, try again.")
+                    continue
+                
+                else:
+                    the_chosen_id = result[int(response_content) - 1]["anime_id"]
+                    break
+                
+        found_anime = AnimeObject(the_chosen_id, Anime.config)
+        
+        embed_1 = discord.Embed(title=f"{found_anime.english_title} `{found_anime.japenese_title}`", url=found_anime.url, color=discord.Colour.red())
+        embed_1.set_author(name="Me!Me!Me!", icon_url=self.client.user.avatar_url)
+        embed_1.set_thumbnail(url=Anime.MAL_LOGO)
+        embed_1.set_image(url=found_anime.cover)
+        embed_1.description = found_anime.synopsis + "\n\n" + found_anime.background
+        
+        embed_2 = discord.Embed(title=f"{found_anime.english_title} `{found_anime.japenese_title}`", url=found_anime.url, color=discord.Colour.red())
+        embed_2.set_author(name="Me!Me!Me!", icon_url=self.client.user.avatar_url)
+        embed_2.set_thumbnail(url=Anime.MAL_LOGO)
+        embed_2.set_image(url=choice(found_anime.pictures))
+        
+        embed_2.add_field(name="Score", value=f"`{found_anime.score}`")
+        embed_2.add_field(name="Rank", value = f"`{found_anime.rank}`")
+        embed_2.add_field(name="Popularity", value = f"`{found_anime.popularity}`")
+        embed_2.add_field(name="Number of episodes", value = f"`{found_anime.number_of_episodes}`")
+        embed_2.add_field(name="Season", value = f"`{found_anime.season}`")
+        embed_2.add_field(name="Broadcast", value = f"`{found_anime.broadcast.capitalize()}`")
+        embed_2.add_field(name="Release", value = f"`{found_anime.release_date}`")
+        embed_2.add_field(name="End", value = f"`{found_anime.end_date}`")  
+        embed_2.add_field(name="Status", value = f"`{found_anime.status.capitalize()}`")
+        embed_2.add_field(name="Age rating", value = f"`{found_anime.age_rating.upper()}`")                
+        embed_2.add_field(name="Genres", value=self.format_list(found_anime.genres))
+        embed_2.add_field(name="Studio(s)", value = self.format_list(found_anime.studios))
+        
+        embed_pages = [embed_1, embed_2]
+        current_page = 0
+        
+        await search_message.edit(content="", embed=embed_1)
+        
+        async def reactions_add(message, reactions):
+            for reaction in reactions:
+                await message.add_reaction(reaction)
+                
+        reactions = {"⬅" : "back", "➡" : "forward"}
+
+        def reaction_check(reaction, user):
+            return (not user.bot) and reaction.message.id == search_message.id and str(reaction) in reactions.keys()
+
+        self.client.loop.create_task(reactions_add(search_message, reactions.keys()))
+        
+        while True:
+
+            try:
+                reaction, user = await self.client.wait_for('reaction_add', timeout=60, check=reaction_check)
+            except asyncio.TimeoutError:
+                await search_message.clear_reactions()
+                return
+            else:
+                reaction_response = reactions[str(reaction)]
+                await search_message.remove_reaction(str(reaction.emoji), user)
+                
+                if reaction_response == "forward":
+                    if not current_page == len(embed_pages) - 1:
+                        current_page += 1
+                    else:
+                        current_page = 0
+                
+                elif reaction_response == "back":
+                    if not current_page == 0:
+                        current_page -= 1
+                    else:
+                        current_page = len(embed_pages) - 1
+                        
+                await search_message.edit(embed=embed_pages[current_page])
+
+def setup(client):
+    client.add_cog(Anime(client))
---
+++ scripts/others/MAL.py
@@ -0,0 +1,191 @@
+from typing import List
+import requests
+
+class MALConfig:
+    def __init__(self, client_id, client_secret, access_token, refresh_token) -> None:
+        self.CLIENT_ID = client_id
+        self.CLIENT_SECRET = client_secret
+        self.ACCESS_TOKEN = access_token
+        self.REFRESH_TOKEN = refresh_token
+        
+        self.headers = {"Authorization": f"Bearer {self.ACCESS_TOKEN}"}
+        
+    def regen_token(self) -> str:
+        request = requests.post("https://myanimelist.net/v1/oauth2/token", data={
+                  "client_id" : self.CLIENT_ID,
+                  "client_secret" : self.CLIENT_SECRET,
+                  "grant_type" : "refresh_token",
+                  "refresh_token" : self.REFRESH_TOKEN
+              })
+        
+        request_data = request.json()
+        
+        self.ACCESS_TOKEN = request_data["access_token"]
+        self.REFRESH_TOKEN = request_data["refresh_token"]
+        
+        return self.ACCESS_TOKEN
+    
+    def get(self, url, params=None, **kwargs) -> requests.Response:
+        if params is None:
+            params = {}
+        
+        return requests.get(url, params=params, headers=self.headers, **kwargs)
+
+    def post(self, url, data, **kwargs) -> requests.Response:
+        return requests.post(url, data=data, headers=self.headers, **kwargs)
+
+class Anime:
+    
+    def __init__(self, _id: str, config: MALConfig) -> None:
+        self._id = _id
+        self._config = config
+        self.reload()
+        
+    @classmethod
+    def from_name(cls, query: str, config: MALConfig):
+        request = config.get("https://api.myanimelist.net/v2/anime", params={"q": query, "limit" : 1})
+        anime_id = str(request.json()["data"][0]["node"]["id"])
+        
+        return cls(_id=anime_id)
+    
+    @classmethod
+    def search(cls, query: str, config: MALConfig, limit: int = 5, basic=True):
+        request = config.get("https://api.myanimelist.net/v2/anime", params={"q": query, "limit" : limit})
+        
+        for anime in request.json()["data"]:
+            data = anime["node"]
+            anime_id = data["id"]
+            
+            if not basic:
+                yield cls(anime_id)
+            else:
+                yield {"name": data["title"], "cover": data["main_picture"]["large"], "anime_id": data["id"]}
+    
+    def reload(self):
+        request = self._config.get(f"https://api.myanimelist.net/v2/anime/{self._id}", params={
+            "fields":"title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,nsfw,created_at,media_type,status,genres,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,studios,statistics"
+            })
+        
+        self._data = request.json()
+        self._data: dict
+    
+    def __str__(self):
+        return self.english_title
+    
+    @property
+    def english_title(self) -> str:
+        return self._data["title"]
+     
+    @property
+    def japenese_title(self) -> str:
+        try:
+            return self._data["alternative_titles"]["ja"]
+        except KeyError:
+            return ""
+    
+    @property
+    def episode_duration(self) -> int:
+        return int(self._data["average_episode_duration"])
+    
+    @property
+    def background(self) -> str:
+        return self._data["background"]
+    
+    @property
+    def broadcast(self) -> str:
+        try:
+            info = self._data["broadcast"]
+        except KeyError:
+            return "NA"
+        
+        return f'{info["day_of_the_week"]} {info["start_time"]}'
+    
+    @property
+    def release_date(self) -> str:
+        return self._data["created_at"].split("T")[0]
+    
+    @property
+    def end_date(self) -> str:
+        try:
+            return self._data["end_date"]
+        except KeyError:
+            return "NA"
+    
+    @property
+    def genres(self) -> List[str]:
+        result = []
+        
+        for i in self._data["genres"]:
+            result.append(i["name"].lower())
+            
+        return result
+    
+    @property
+    def cover(self) -> str:
+        return self._data["main_picture"]["large"]
+    
+    @property
+    def pictures(self) -> List[str]:
+        result = []
+        
+        for i in self._data["pictures"]:
+            result.append(i["large"])
+        
+        if result == []:
+            result == [self.cover]
+        return result
+    
+    @property
+    def score(self) -> float:
+        return float(self._data["mean"])
+    
+    @property
+    def number_of_episodes(self) -> int:
+        return int(self._data["num_episodes"])
+    
+    @property
+    def popularity(self) -> int:
+        return int(self._data["popularity"])
+                   
+    @property
+    def rank(self) -> int:
+        return int(self._data["rank"])
+        
+    @property
+    def age_rating(self) -> str:
+        return self._data["rating"]
+    
+    @property
+    def source(self) -> str:
+        return self._data["source"]
+    
+    @property
+    def season(self) -> str:
+        info = self._data["start_season"]
+        
+        return f"{info['season'].capitalize()} {str(info['year'])}"
+    
+    @property
+    def studios(self) -> List[str]:
+        result = []
+        
+        for i in self._data["studios"]:
+            result.append(i["name"])
+            
+        return result
+    
+    @property
+    def status(self) -> str:
+        return self._data["status"].replace("_", " ")
+    
+    @property
+    def synopsis(self) -> str:
+        return self._data["synopsis"]
+    
+    @property
+    def nsfw(self) -> str:
+        return self._data["nsfw"]
+    
+    @property
+    def url(self) -> str:
+        return f"https://myanimelist.net/anime/{self._id}/{self.english_title.replace(' ', '_')}"
