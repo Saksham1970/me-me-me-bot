f09a0c289ba7ca72b5f6c531a145611eeff625d7
Priyanshu <priyanv2003@gmail.com>
2020-12-18 18:32:32 +0530

    Added manga command

--- scripts/bot/cogs/anime.py
+++ scripts/bot/cogs/anime.py
@@ -8,11 +8,11 @@ import discord
 import asyncio
 from random import choice
 from discord.ext import commands
-from MAL import Anime as AnimeObject, MALConfig
+from MAL import Anime, Manga, MALConfig
 from dotenv import load_dotenv
 load_dotenv()
 
-class Anime(commands.Cog):
+class Weeb(commands.Cog):
       
     config = MALConfig(
     client_id = os.environ.get("MAL_CLIENT_ID"),
@@ -36,85 +36,38 @@ class Anime(commands.Cog):
                 result += f"`{j}`"
                 
         return result
-        
-    @commands.command()
-    @commands.cooldown(rate=3, per=4, type=BucketType.member)
-    async def anime(self, ctx, *, query):
-        result = list(AnimeObject.search(query=query, limit=10, basic=True, config=Anime.config))
-        if len(result) == 0:
-            await ctx.send(f"No anime of name `{query}` found.")
-            return
-        
-        msg_content = "Respond with the index of the anime you want, 'c' to cancel\n"
-        for index, anime in enumerate(result):
-            anime_name = anime["name"]
-            msg_content += f"{index + 1}. **{anime_name}**\n"
-        
-        search_message = await ctx.send(msg_content)
-        
-        def check(message) -> bool:
-            return message.author == ctx.author and ((message.content.isdigit() or message.content[1:].isdigit()) or message.content.lower() == "c")
-        
-        the_chosen_id = None
-        errors_commited = 0
-        
-        while True:
-              
-            if errors_commited >= 3:
-                await search_message.edit(content="Bruh you are too retarded I give up")
-                return
-                
-            try:
-                response = await self.client.wait_for("message", check=check, timeout=30)
-            except asyncio.TimeoutError:
-                await ctx.send("I got no response sadly, try refining your search term if you didn't find your anime.")
-                return
-            else:
-                response_content = response.content.lower()
-              
-                if response_content == "c":
-                    await search_message.edit(content="Command cancelled, try refining your search term if you didn't find your anime.")
-                    return
-                
-                elif int(response_content) <= 0:
-                    await ctx.send("Respond with a natural number, should have been obvious enuff.")
-                    errors_commited += 1
-                    continue
-                
-                elif int(response_content) > len(result):
-                    await ctx.send("I dont even have that many results bro, try again.")
-                    errors_commited += 1
-                    continue
-                
-                else:
-                    the_chosen_id = result[int(response_content) - 1]["anime_id"]
-                    break
-                
-        found_anime = AnimeObject(the_chosen_id, Anime.config)
-        
-        embed_1 = discord.Embed(title=f"{found_anime.english_title} `{found_anime.japenese_title}`", url=found_anime.url, color=discord.Colour.red())
+    
+    async def weeb_embed(self, ctx, search_message, weeb_abc, type):
+        embed_1 = discord.Embed(title=f"{weeb_abc.english_title} `{weeb_abc.japenese_title}`", url=weeb_abc.url, color=discord.Colour.red())
         embed_1.set_author(name="Me!Me!Me!", icon_url=self.client.user.avatar_url)
-        embed_1.set_thumbnail(url=Anime.MAL_LOGO)
-        embed_1.set_image(url=found_anime.cover)
-        embed_1.description = found_anime.synopsis + "\n\n" + found_anime.background
+        embed_1.set_thumbnail(url=Weeb.MAL_LOGO)
+        embed_1.set_image(url=weeb_abc.cover)
+        embed_1.description = weeb_abc.synopsis + "\n\n" + weeb_abc.background
         
-        embed_2 = discord.Embed(title=f"{found_anime.english_title} `{found_anime.japenese_title}`", url=found_anime.url, color=discord.Colour.red())
+        embed_2 = discord.Embed(title=f"{weeb_abc.english_title} `{weeb_abc.japenese_title}`", url=weeb_abc.url, color=discord.Colour.red())
         embed_2.set_author(name="Me!Me!Me!", icon_url=self.client.user.avatar_url)
-        embed_2.set_thumbnail(url=Anime.MAL_LOGO)
-        embed_2.set_image(url=choice(found_anime.pictures))
+        embed_2.set_thumbnail(url=Weeb.MAL_LOGO)
+        embed_2.set_image(url=choice(weeb_abc.pictures))
         
-        embed_2.add_field(name="Score", value=f"`{found_anime.score}`")
-        embed_2.add_field(name="Rank", value = f"`{found_anime.rank}`")
-        embed_2.add_field(name="Popularity", value = f"`{found_anime.popularity}`")
-        embed_2.add_field(name="Number of episodes", value = f"`{found_anime.number_of_episodes}`")
-        embed_2.add_field(name="Season", value = f"`{found_anime.season}`")
-        embed_2.add_field(name="Broadcast", value = f"`{found_anime.broadcast.capitalize()}`")
-        embed_2.add_field(name="Release", value = f"`{found_anime.release_date}`")
-        embed_2.add_field(name="End", value = f"`{found_anime.end_date}`")  
-        embed_2.add_field(name="Status", value = f"`{found_anime.status.capitalize()}`")
-        embed_2.add_field(name="Age rating", value = f"`{found_anime.age_rating.upper()}`")                
-        embed_2.add_field(name="Genres", value=self.format_list(found_anime.genres))
-        embed_2.add_field(name="Studio(s)", value = self.format_list(found_anime.studios))
+        embed_2.add_field(name="Score", value=f"`{weeb_abc.score}`")
+        embed_2.add_field(name="Rank", value = f"`{weeb_abc.rank}`")
+        embed_2.add_field(name="Popularity", value = f"`{weeb_abc.popularity}`")
+        
+        if type == "anime":
+            embed_2.add_field(name="Number of episodes", value = f"`{weeb_abc.number_of_episodes}`")
+            embed_2.add_field(name="Season", value = f"`{weeb_abc.season}`")
+            embed_2.add_field(name="Broadcast", value = f"`{weeb_abc.broadcast.capitalize()}`")
+            embed_2.add_field(name="Studio(s)", value = self.format_list(weeb_abc.studios))
+            embed_2.add_field(name="Age rating", value = f"`{weeb_abc.age_rating.upper()}`")                
+        elif type == "manga":
+            embed_2.add_field(name="Number of volumes", value = f"`{weeb_abc.number_of_volumes}`")
+            embed_2.add_field(name="Number of chapters", value = f"`{weeb_abc.number_of_chapters}`")
+            embed_2.add_field(name="Author(s)", value = self.format_list(weeb_abc.authors))
+            
+        embed_2.add_field(name="Release", value = f"`{weeb_abc.release_date}`")
+        embed_2.add_field(name="End", value = f"`{weeb_abc.end_date}`")  
+        embed_2.add_field(name="Status", value = f"`{weeb_abc.status.capitalize()}`")
+        embed_2.add_field(name="Genres", value=self.format_list(weeb_abc.genres))
         
         embed_pages = [embed_1, embed_2]
         current_page = 0
@@ -156,6 +109,144 @@ class Anime(commands.Cog):
                         current_page = len(embed_pages) - 1
                         
                 await search_message.edit(embed=embed_pages[current_page])
+        
+    @commands.command()
+    @commands.cooldown(rate=3, per=4, type=BucketType.member)
+    async def anime(self, ctx, *, query):
+        result = list(Anime.search(query=query, limit=10, basic=True, config=Weeb.config))
+        if len(result) == 0:
+            await ctx.send(f"No anime of name `{query}` found.")
+            return
+        
+        msg_content = "Respond with the index of the anime you want, 'c' to cancel\n"
+        for index, anime in enumerate(result):
+            anime_name = anime["name"]
+            msg_content += f"{index + 1}. **{anime_name}**\n"
+        
+        search_message = await ctx.send(msg_content)
+        
+        def check(message) -> bool:
+            return message.author == ctx.author and ((message.content.isdigit() or message.content[1:].isdigit()) or message.content.lower() == "c")
+        
+        the_chosen_id = None
+        errors_commited = 0
+        
+        while True:
+              
+            if errors_commited >= 3:
+                await search_message.edit(content="Bruh you are too retarded I give up")
+                return
+                
+            try:
+                response = await self.client.wait_for("message", check=check, timeout=30)
+            except asyncio.TimeoutError:
+                await ctx.send("I got no response sadly, try refining your search term if you didn't find your anime.")
+                return
+            else:
+                response_content = response.content.lower()
+              
+                if response_content == "c":
+                    await search_message.edit(content="Command cancelled, try refining your search term if you didn't find your anime.")
+                    
+                    try:
+                        await response.delete(delay=3)
+                    except discord.Forbidden:
+                        pass
+                    
+                    return
+                
+                elif int(response_content) <= 0:
+                    await ctx.send("Respond with a natural number, should have been obvious enuff.")
+                    errors_commited += 1
+                    continue
+                
+                elif int(response_content) > len(result):
+                    await ctx.send("I dont even have that many results bro, try again.")
+                    errors_commited += 1
+                    continue
+                
+                else:
+                    the_chosen_id = result[int(response_content) - 1]["anime_id"]
+                    
+                    try:
+                        await response.delete(delay=3)
+                    except discord.Forbidden:
+                        pass
+                    
+                    break
+                
+        found_anime = Anime(the_chosen_id, Weeb.config)
+        await self.weeb_embed(ctx=ctx, search_message=search_message, weeb_abc=found_anime, type="anime")
+        
+    @commands.command()
+    @commands.cooldown(rate=3, per=4, type=BucketType.member)
+    async def manga(self, ctx, *, query):
+        result = list(Manga.search(query=query, limit=10, basic=True, config=Weeb.config))
+        if len(result) == 0:
+            await ctx.send(f"No manga of name `{query}` found.")
+            return
+        
+        msg_content = "Respond with the index of the manga you want, 'c' to cancel\n"
+        for index, manga in enumerate(result):
+            manga_name = manga["name"]
+            msg_content += f"{index + 1}. **{manga_name}**\n"
+        
+        search_message = await ctx.send(msg_content)
+        
+        def check(message) -> bool:
+            return message.author == ctx.author and ((message.content.isdigit() or message.content[1:].isdigit()) or message.content.lower() == "c")
+        
+        the_chosen_id = None
+        errors_commited = 0
+        
+        while True:
+                
+            if errors_commited >= 3:
+                await search_message.edit(content="Bruh you are too retarded I give up")
+                return
+                
+            try:
+                response = await self.client.wait_for("message", check=check, timeout=30)
+            except asyncio.TimeoutError:
+                await ctx.send("I got no response sadly, try refining your search term if you didn't find your anime.")
+                return
+            else:
+                response_content = response.content.lower()
+                
+                if response_content == "c":
+                    await search_message.edit(content="Command cancelled, try refining your search term if you didn't find your manga.")
+                    
+                    try:
+                        await response.delete(delay=3)
+                    except discord.Forbidden:
+                        pass
+                    
+                    return
+                
+                elif int(response_content) <= 0:
+                    await ctx.send("Respond with a natural number, should have been obvious enuff.")
+                    errors_commited += 1
+                    continue
+                
+                elif int(response_content) > len(result):
+                    await ctx.send("I dont even have that many results bro, try again.")
+                    errors_commited += 1
+                    continue
+                
+                else:
+                    the_chosen_id = result[int(response_content) - 1]["manga_id"]
+                    
+                    try:
+                        await response.delete(delay=3)
+                    except discord.Forbidden:
+                        pass
+                    
+                    break
+                
+        found_anime = Manga(the_chosen_id, Weeb.config)
+        await self.weeb_embed(ctx=ctx, search_message=search_message, weeb_abc=found_anime, type="manga")
+        
+        
 
 def setup(client):
-    client.add_cog(Anime(client))
+    client.add_cog(Weeb(client))
--- scripts/others/MAL.py
+++ scripts/others/MAL.py
@@ -1,5 +1,6 @@
 from typing import List
 import requests
+
 class MALConfig:
     def __init__(self, client_id, client_secret, access_token, refresh_token) -> None:
         self.CLIENT_ID = client_id
@@ -7,7 +8,18 @@ class MALConfig:
         self.ACCESS_TOKEN = access_token
         self.REFRESH_TOKEN = refresh_token
         
-        self.headers = {"Authorization": f"Bearer {self.ACCESS_TOKEN}"}
+    @property
+    def headers(self):
+        return {"Authorization": f"Bearer {self.ACCESS_TOKEN}"}
+    
+    @classmethod
+    def empty(cls):
+        return cls(
+            client_id="",
+            client_secret="",
+            access_token="",
+            refresh_token=""
+        )
         
     def regen_token(self) -> str:
         request = requests.post("https://myanimelist.net/v1/oauth2/token", data={
@@ -32,44 +44,12 @@ class MALConfig:
 
     def post(self, url, data, **kwargs) -> requests.Response:
         return requests.post(url, data=data, headers=self.headers, **kwargs)
-
-class Anime:
     
-    def __init__(self, _id: str, config: MALConfig) -> None:
-        self._id = _id
-        self._config = config
-        self.reload()
+class abc:
+    
+    def __init__(self, data) -> None:
+        self._data = data
         
-    @classmethod
-    def from_name(cls, query: str, config: MALConfig):
-        request = config.get("https://api.myanimelist.net/v2/anime", params={"q": query, "limit" : 1})
-        anime_id = str(request.json()["data"][0]["node"]["id"])
-        return cls(_id=anime_id, config=config)
-    
-    @classmethod
-    def search(cls, query: str, config: MALConfig, limit: int = 5, basic=True):
-        request = config.get("https://api.myanimelist.net/v2/anime", params={"q": query, "limit" : limit})
-        
-        for anime in request.json()["data"]:
-            data = anime["node"]
-            anime_id = data["id"]
-            
-            if not basic:
-                yield cls(anime_id)
-            else:
-                yield {"name": data["title"], "cover": data["main_picture"]["large"], "anime_id": data["id"]}
-    
-    def reload(self):
-        request = self._config.get(f"https://api.myanimelist.net/v2/anime/{self._id}", params={
-            "fields":"title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,nsfw,created_at,media_type,status,genres,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,studios,statistics"
-            })
-        
-        self._data = request.json()
-        self._data: dict
-    
-    def __str__(self):
-        return self.english_title
-    
     @property
     def english_title(self) -> str:
         return self._data["title"]
@@ -80,10 +60,6 @@ class Anime:
             return self._data["alternative_titles"]["ja"]
         except KeyError:
             return ""
-    
-    @property
-    def episode_duration(self) -> int:
-        return int(self._data["average_episode_duration"])
     
     @property
     def background(self) -> str:
@@ -138,39 +114,12 @@ class Anime:
         return float(self._data["mean"])
     
     @property
-    def number_of_episodes(self) -> int:
-        return int(self._data["num_episodes"])
-    
-    @property
     def popularity(self) -> int:
         return int(self._data["popularity"])
                    
     @property
     def rank(self) -> int:
         return int(self._data["rank"])
-        
-    @property
-    def age_rating(self) -> str:
-        return self._data["rating"]
-    
-    @property
-    def source(self) -> str:
-        return self._data["source"]
-    
-    @property
-    def season(self) -> str:
-        info = self._data["start_season"]
-        
-        return f"{info['season'].capitalize()} {str(info['year'])}"
-    
-    @property
-    def studios(self) -> List[str]:
-        result = []
-        
-        for i in self._data["studios"]:
-            result.append(i["name"])
-            
-        return result
     
     @property
     def status(self) -> str:
@@ -183,7 +132,134 @@ class Anime:
     @property
     def nsfw(self) -> str:
         return self._data["nsfw"]
+
+class Anime(abc):
+    
+    def __init__(self, _id: str, config: MALConfig) -> None:
+        self._id = _id
+        self._config = config
+        self.reload()
+        
+    @classmethod
+    def from_name(cls, query: str, config: MALConfig):
+        request = config.get("https://api.myanimelist.net/v2/anime", params={"q": query, "limit" : 1})
+        anime_id = str(request.json()["data"][0]["node"]["id"])
+        return cls(_id=anime_id, config=config)
+    
+    @classmethod
+    def search(cls, query: str, config: MALConfig, limit: int = 5, basic=True):
+        request = config.get("https://api.myanimelist.net/v2/anime", params={"q": query, "limit" : limit})
+    
+        for anime in request.json()["data"]:
+            data = anime["node"]
+            anime_id = data["id"]
+            
+            if not basic:
+                yield cls(anime_id, config)
+            else:
+                yield {"name": data["title"], "cover": data["main_picture"]["large"], "anime_id": data["id"]}
+    
+    def reload(self):
+        request = self._config.get(f"https://api.myanimelist.net/v2/anime/{self._id}", params={
+            "fields":"title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,nsfw,created_at,media_type,status,genres,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,studios"
+            })
+        
+        super().__init__(data=request.json())
+    
+    def __str__(self):
+        return self.english_title
     
     @property
     def url(self) -> str:
         return f"https://myanimelist.net/anime/{self._id}/{self.english_title.replace(' ', '_')}"
+    
+    @property
+    def episode_duration(self) -> int:
+        return int(self._data["average_episode_duration"])
+    
+    @property
+    def number_of_episodes(self) -> int:
+        return int(self._data["num_episodes"])
+    
+    @property
+    def studios(self) -> List[str]:
+        result = []
+        
+        for i in self._data["studios"]:
+            result.append(i["name"])
+            
+        return result
+    
+    @property
+    def source(self) -> str:
+        return self._data["source"]
+    
+    @property
+    def season(self) -> str:
+        info = self._data["start_season"]
+        
+        return f"{info['season'].capitalize()} {str(info['year'])}"
+    
+    @property
+    def age_rating(self) -> str:
+        return self._data["rating"]
+        
+    
+class Manga(abc):
+    
+    def __init__(self, _id: str, config: MALConfig) -> None:
+        self._id = _id
+        self._config = config
+        self.reload()
+        
+    @classmethod
+    def from_name(cls, query: str, config: MALConfig):
+        request = config.get("https://api.myanimelist.net/v2/manga", params={"q": query, "limit" : 1})
+        anime_id = str(request.json()["data"][0]["node"]["id"])
+        return cls(_id=anime_id, config=config)
+    
+    @classmethod
+    def search(cls, query: str, config: MALConfig, limit: int = 5, basic=True):
+        request = config.get("https://api.myanimelist.net/v2/manga", params={"q": query, "limit" : limit})
+    
+        for manga in request.json()["data"]:
+            data = manga["node"]
+            manga_id = data["id"]
+            
+            if not basic:
+                yield cls(manga_id, config)
+            else:
+                yield {"name": data["title"], "cover": data["main_picture"]["large"], "manga_id": data["id"]}
+    
+    def reload(self):
+        request = self._config.get(f"https://api.myanimelist.net/v2/manga/{self._id}", params={
+            "fields":"title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,nsfw,created_at,media_type,status,genres,pictures,background,studios,num_volumes,num_chapters,authors"
+            })
+        
+        super().__init__(data=request.json())
+    
+    def __str__(self):
+        return self.english_title
+    
+    @property
+    def url(self) -> str:
+        return f"https://myanimelist.net/manga/{self._id}/{self.english_title.replace(' ', '_')}"
+    
+    @property
+    def number_of_volumes(self) -> int:
+        return int(self._data["num_volumes"])
+    
+    @property
+    def number_of_chapters(self) -> int:
+        return int(self._data["num_chapters"])
+    
+    @property
+    def authors(self) -> List[str]:
+        result = []
+        
+        for i in self._data["authors"]:
+            if "first_name" in i["node"]:
+                name = i["node"]["first_name"] + " " + i["node"]["last_name"]
+                result.append(name)
+            
+        return result if len(result) != 0 else ["NA"]
